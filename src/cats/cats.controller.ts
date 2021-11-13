22f3a308362d5eb4b847ea2ef5a3242ed9cb909e
SakaiTaka23 <tt3.sakai@gmail.com>
2021-11-13 10:03:48 +0900

    コントローラーに関する学習をした

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,7 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  @HttpCode(200)
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
+  @Get(':id')
+  // findOne(@Param() params): string {
+  //   return `This action returns a #${params.id} cat`;
+  // }
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} cat`;
+  }
+
   @Get()
   findAll(): string {
     return 'This action returns all cats';

c1415246f28b4b8f01bb65849e234bd42b067149
SakaiTaka23 <tt3.sakai@gmail.com>
2021-11-14 20:19:10 +0900

    ドキュメントから動画へ切り替え

--- graphql/src/app.module.ts
+++ graphql/src/app.module.ts
@@ -1,21 +1,10 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
-import { join } from 'path/posix';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PetsModule } from './pets/pets.module';
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot({
-      playground: false,
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
-      typePaths: ['./**/*.graphql'],
-      definitions: {
-        path: join(process.cwd(), 'src/graphql.ts'),
-      },
-    }),
-  ],
+  imports: [PetsModule],
   controllers: [AppController],
   providers: [AppService],
 })

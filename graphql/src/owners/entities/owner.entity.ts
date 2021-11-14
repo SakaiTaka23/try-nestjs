e108a553e516cc86ff60406a95bc3f2ca159a349
SakaiTaka23 <tt3.sakai@gmail.com>
2021-11-14 21:45:26 +0900

    ownerのcreate,read実装

--- graphql/src/owners/entities/owner.entity.ts
+++ graphql/src/owners/entities/owner.entity.ts
@@ -1,5 +1,6 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Pet } from 'src/pets/pet.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 @ObjectType()
@@ -11,4 +12,8 @@ export class Owner {
   @Column()
   @Field()
   name: string;
+
+  @OneToMany(() => Pet, (pet) => pet.owner)
+  @Field((type) => [Pet], { nullable: true })
+  pets?: Pet[];
 }

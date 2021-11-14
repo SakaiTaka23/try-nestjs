c3faf9ef3dfd55481724e911799e418737284a0f
SakaiTaka23 <tt3.sakai@gmail.com>
2021-11-14 14:55:11 +0900

    graphql serverのための準備をした

--- graphql/README.md
+++ graphql/README.md
@@ -1,4 +1,19 @@
 # graphql-nest
 
 - [graphql-nest](#graphql-nest)
+- [Quick start](#quick-start)
+  - [Getting started with GraphQL & TypeScript](#getting-started-with-graphql--typescript)
 
+
+
+# Quick start
+- Nestではコード駆動・スキーマ駆動の2つから開発ができる
+- コードベース デコレーターやクラスを使ってスキーマを作る
+- スキーマベース SDLによって開発を進める スキーマからTypeScriptのコードを生成
+- 個人的にスキーマベースの方が好き
+
+
+
+## Getting started with GraphQL & TypeScript
+- 最上位のモジュールにGraphQLModuleをインポートして設定を書いていく
+- スキーマ駆動の場合どこにgraqhqlファイルを置いたのか指定する
--- graphql/src/app.module.ts
+++ graphql/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { join } from 'path/posix';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

29332619371cbc5cb6ebd168f1ff2cf451983e15
SakaiTaka23 <tt3.sakai@gmail.com>
2021-11-17 19:32:51 +0900

    外部から設定をするようにした

--- nest-typeorm/src/app.module.ts
+++ nest-typeorm/src/app.module.ts
@@ -3,20 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { User } from './user/entities/user.entity';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: ':memory:',
-      entities: [User],
-      synchronize: true,
-    }),
-    // TypeOrmModule.forRoot(),
-    UserModule,
-  ],
+  imports: [TypeOrmModule.forRoot(), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })

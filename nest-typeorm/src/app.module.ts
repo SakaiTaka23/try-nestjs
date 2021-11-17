b7bc2cd511d3d0704bf7b475a2b6ccd75853e94d
SakaiTaka23 <tt3.sakai@gmail.com>
2021-11-17 19:26:46 +0900

    ormの設定をした

--- nest-typeorm/src/app.module.ts
+++ nest-typeorm/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './user/entities/user.entity';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      entities: [User],
+      synchronize: true,
+    }),
+    // TypeOrmModule.forRoot(),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}

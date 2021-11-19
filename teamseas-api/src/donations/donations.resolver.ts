deba12d3fffc87bb4de0c13c98f906dca4969342
SakaiTaka23 <tt3.sakai@gmail.com>
2021-11-19 14:08:07 +0900

    create, findoneを実装

--- teamseas-api/src/donations/donations.resolver.ts
+++ teamseas-api/src/donations/donations.resolver.ts
@@ -1,9 +1,18 @@
-import { Resolver, Query, Args } from '@nestjs/graphql';
+import { Prisma } from '.prisma/client';
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { DonationsService } from './donations.service';
 
 @Resolver('Donation')
 export class DonationsResolver {
   constructor(private readonly donationsService: DonationsService) {}
+
+  @Mutation('createDonation')
+  create(
+    @Args('createDonationInput')
+    createDonationInput: Prisma.DonationCreateInput,
+  ) {
+    return this.donationsService.create(createDonationInput);
+  }
 
   @Query('donations')
   findAll() {
@@ -12,6 +21,6 @@ export class DonationsResolver {
 
   @Query('donation')
   findOne(@Args('id') id: number) {
-    return this.donationsService.findOne(id);
+    return this.donationsService.findOne({ id });
   }
 }
--- teamseas-api/src/donations/donations.service.ts
+++ teamseas-api/src/donations/donations.service.ts
@@ -1,20 +1,24 @@
+import { Prisma } from '.prisma/client';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
-import { CreateDonationInput } from './dto/create-donation.input';
 
 @Injectable()
 export class DonationsService {
   constructor(private prisma: PrismaService) {}
 
-  create(createDonationInput: CreateDonationInput) {
-    return 'This action adds a new donation';
+  create(createDonationInput: Prisma.DonationCreateInput) {
+    return this.prisma.donation.create({
+      data: createDonationInput,
+    });
   }
 
   findAll() {
     return this.prisma.donation.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} donation`;
+  findOne(donationWhereUniqueInput: Prisma.DonationWhereUniqueInput) {
+    return this.prisma.donation.findUnique({
+      where: donationWhereUniqueInput,
+    });
   }
 }

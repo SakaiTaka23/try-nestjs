bfaaeef1a1b742b6ca3ed70e7dbaf7c65916fa14
SakaiTaka23 <tt3.sakai@gmail.com>
2021-12-18 20:27:54 +0900

    生成した型を使った形に修正

--- grpc/src/hero/heros.controller.ts
+++ grpc/src/hero/heros.controller.ts
@@ -1,15 +1,19 @@
 import { Controller } from '@nestjs/common';
-import { GrpcMethod } from '@nestjs/microservices';
-import { HeroById } from './interfaces/hero-by-id.interface';
-import { Hero } from './interfaces/hero.interface';
+import {
+  Hero,
+  HeroById,
+  HeroesServiceController,
+  HeroesServiceControllerMethods,
+} from './hero';
 
-@Controller()
-export class HeroesController {
+@HeroesServiceControllerMethods()
+@Controller('hero')
+export class HeroesController implements HeroesServiceController {
   private readonly items: Hero[] = [
     { id: 1, name: 'John' },
     { id: 2, name: 'Doe' },
   ];
-  @GrpcMethod('HeroesService')
+
   findOne(data: HeroById): Hero {
     return this.items.find(({ id }) => id === data.id);
   }

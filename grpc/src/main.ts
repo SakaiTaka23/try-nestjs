8e6141a599cd146cbc9c580600708c5ea525fb68
SakaiTaka23 <tt3.sakai@gmail.com>
2021-12-18 18:41:58 +0900

    コードを分割

--- grpc/src/main.ts
+++ grpc/src/main.ts
@@ -1,19 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import {
+  GrpcOptions,
+  MicroserviceOptions,
+  Transport,
+} from '@nestjs/microservices';
 import { join } from 'path/posix';
 import { AppModule } from './app.module';
+
+const gRPCOption: GrpcOptions = {
+  transport: Transport.GRPC,
+  options: {
+    package: 'hero',
+    protoPath: join(__dirname, 'hero/hero.proto'),
+    url: '127.0.0.1:5000',
+  },
+};
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
-    {
-      transport: Transport.GRPC,
-      options: {
-        package: 'hero',
-        protoPath: join(__dirname, 'hero/hero.proto'),
-        url: '127.0.0.1:5000',
-      },
-    },
+    gRPCOption,
   );
   await app.listen();
 }

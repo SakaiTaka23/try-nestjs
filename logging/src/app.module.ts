f6286e8e43c3d80832692e13aa0b44a70f54c7e0
SakaiTaka23 <tt3.sakai@gmail.com>
2022-06-12 10:45:32 +0900

    ログが出ることを確認

--- logging/src/app.module.ts
+++ logging/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MyLoggerService } from './my-logger/my-logger.service';
+import { MyLoggerModule } from './my-logger/my-logger.module';
 
 @Module({
-  imports: [],
+  imports: [MyLoggerModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MyLoggerService],
 })
 export class AppModule {}
---
+++ logging/src/my-logger/my-logger.service.ts
@@ -0,0 +1,42 @@
+import { LoggerService } from '@nestjs/common';
+import { createLogger, format, Logger, transports } from 'winston';
+
+export class MyLoggerService implements LoggerService {
+  logger: Logger;
+
+  constructor() {
+    this.logger = createLogger({
+      level: 'info',
+      format: format.combine(format.timestamp(), format.json()),
+      defaultMeta: { service: 'user-service' },
+      transports: [
+        //
+        // - Write all logs with level `error` and below to `error.log`
+        // - Write all logs with level `info` and below to `combined.log`
+        //
+        new transports.File({ filename: 'error.log', level: 'error' }),
+        new transports.File({ filename: 'combined.log' }),
+      ],
+    });
+  }
+
+  log(message: string) {
+    this.logger.info(message);
+  }
+
+  error(message: string, trace: string) {
+    this.logger.error(message, trace);
+  }
+
+  warn(message: string) {
+    this.logger.warn(message);
+  }
+
+  debug(message: string) {
+    this.logger.debug(message);
+  }
+
+  verbose(message: string) {
+    this.logger.verbose(message);
+  }
+}

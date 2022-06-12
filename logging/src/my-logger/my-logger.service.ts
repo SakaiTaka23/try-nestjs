c713b922d0bc56164a861d1c5c481766d40f907e
SakaiTaka23 <tt3.sakai@gmail.com>
2022-06-12 10:58:28 +0900

    コンソールにも表示するようにした

--- logging/src/my-logger/my-logger.service.ts
+++ logging/src/my-logger/my-logger.service.ts
@@ -16,6 +16,9 @@ export class MyLoggerService implements LoggerService {
         //
         new transports.File({ filename: 'error.log', level: 'error' }),
         new transports.File({ filename: 'combined.log' }),
+        new transports.Console({
+          format: format.combine(format.colorize(), format.simple()),
+        }),
       ],
     });
   }
